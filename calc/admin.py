d18ad1760b751888c68bc51e0f592bcefca922b7
Bhaveshkew <bhaveshkew11@gmail.com>
2022-06-22 11:58:22 +0530

    second

--- calc/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from calc.models import Person , RegisteredChild,User
-# Register your models here.
-
-admin.site.register(Person)
-admin.site.register(RegisteredChild)
-admin.site.register(User)
